07155f44c6f4b3c34abffcdc02d2deb108cbd6fe
Zeth Kane <zkane@zipcodes-MacBook-Pro-9.local>
2020-03-09 12:29:48 -0400

    almost got wavetest

--- src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
@@ -5,14 +5,20 @@ package rocks.zipcode.io.quiz3.arrays;
  */
 public class WaveGenerator {
     public static String[] wave(String str) {
-        String[] wavey = new String[str.length()];
         String newStr =  str.toLowerCase();
+        String[] wavey = newStr.split("");
+        String[] splitted = newStr.split("");
         String finalString = "";
-        for (int i = 0; i < wavey.length; i++) {
-            finalString = newStr.replace(newStr.charAt(i), 'A');
-            wavey[i] = finalString;
-            finalString = "";
-        }
-        return wavey;
+        finalString += newStr;
+        for (int i = 0; i < splitted.length ; i++) {
+            for (int j = 0; j < splitted.length - 1 ; j++) {
+                if( i == j){
+                    splitted[i] += newStr.toUpperCase().charAt(i);
+                }
+                splitted[i] += wavey[j];
+            }
+            }
+
+        return splitted;
     }
 }
