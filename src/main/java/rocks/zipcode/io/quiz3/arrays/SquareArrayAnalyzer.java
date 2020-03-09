e070316a37fb04595f0f08d0987a073506120161
Zeth Kane <zkane@zipcodes-MacBook-Pro-9.local>
2020-03-09 09:54:20 -0400

    sqaure array done and some of tictactoe

--- src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
@@ -14,6 +14,16 @@ public class SquareArrayAnalyzer {
      * @return true if each `inputArray[i]` has a corresponding `squaredValues[j]` such that `squaredValues[j] == inputArray[i] * inputArray[i]`
      */
     public static Boolean compare(Integer[] inputArray, Integer[] squaredValues) {
-        return null;
+        for (int i = 0; i < inputArray.length ; i++) {
+            for (int j = 0; j < squaredValues.length; j++) {
+                if (inputArray[i] * inputArray[i] == squaredValues[j]) {
+                    return true;
+                }
+
+            }
+
+        } return false;
+
+
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
@@ -5,18 +5,59 @@ package rocks.zipcode.io.quiz3.arrays;
  */
 public class TicTacToe {
 
+    String[][] board;
+
     public TicTacToe(String[][] board) {
+        this.board = board;
     }
 
     public TicTacToe() {
     }
 
     public String[] getRow(Integer value) {
-        return null;
+        String[] row = new String[3];
+        String first = "";
+        String second = "";
+        String third = "";
+        first += board[value][0];
+        second += board[value][1];
+        third += board[value][2];
+        for (int i = 0; i <row.length ; i++) {
+            if (i == 0){
+                row[i] = first;
+            }
+            if( i == 1){
+                row[i] = second;
+            }
+            if( i == 2){
+                row[i] = third;
+            }
+        }
+
+        return row;
     }
 
     public String[] getColumn(Integer value) {
-        return null;
+        String[] column = new String[3];
+        String top = "";
+        String middle = "";
+        String bottem = "";
+        top += board[0][value];
+        middle += board[1][value];
+        bottem += board[2][value];
+        for (int i = 0; i <column.length ; i++) {
+            if (i == 0){
+                column[i] = top;
+            }
+            if( i == 1){
+                column[i] = middle;
+            }
+            if( i == 2){
+                column[i] = bottem;
+            }
+        }
+
+        return column;
     }
 
     public Boolean isRowHomogenous(Integer rowIndex) {
@@ -32,6 +73,6 @@ public class TicTacToe {
     }
 
     public String[][] getBoard() {
-        return null;
+        return this.board;
     }
 }
